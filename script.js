6e9c7e752c17abd191933b7747bd6c99161a1728
Rivj960229 <157336297+Rivj960229@users.noreply.github.com>
2025-02-18 11:37:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let rolSeleccionado = "";
+
+function seleccionarRol(rol) {
+    rolSeleccionado = rol;
+    document.getElementById('seleccionRol').style.display = 'none';
+    document.getElementById('formulario').style.display = 'flex';
+    document.getElementById('asesor').value = rol + ": ";
+    document.getElementById('asesor').disabled = false;
+}
+
 function copiarAlPortapapeles() {
-    const datos = `Número a portar: ${document.getElementById('numeroPortar').value}\n` +
+    const nombreIngresado = document.getElementById('asesor').value.replace(rolSeleccionado + ": ", "");
+    const datos = `${rolSeleccionado}: ${nombreIngresado}\n` +
+                  `Número a portar: ${document.getElementById('numeroPortar').value}\n` +
                   `NIP: ${document.getElementById('nip').value}\n` +
                   `Zona: ${document.getElementById('zona').value}\n` +
-                  `Asesor: ${document.getElementById('asesor').value}\n` +
                   `ICC: ${document.getElementById('icc').value}`;
     navigator.clipboard.writeText(datos).then(() => {
         alert("Datos copiados al portapapeles");
@@ -14,5 +25,9 @@ function nuevaVenta() {
     document.getElementById('nip').value = "";
     document.getElementById('zona').value = "";
     document.getElementById('asesor').value = "";
+    document.getElementById('asesor').disabled = true;
     document.getElementById('icc').value = "";
+    document.getElementById('seleccionRol').style.display = 'flex';
+    document.getElementById('formulario').style.display = 'none';
 }
+
